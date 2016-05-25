6a4be52be0ad096757c8d34b6c501d6fdfd483fb
Alan Vaudagna <alanvaudagna@gmail.com>
2016-05-25 18:58:24 -0300

    Added deserialize_pcb and comments.

--- vac-commons/pcb/pcb.c
+++ vac-commons/pcb/pcb.c
@@ -1,8 +1,13 @@
 
 #include "pcb.h"
 
-void serialize_enum_queue(enum_queue status, char **pString);
-
+/*
+ * Serializa un pcb a una cadena de bytes.
+ *
+ * pcb : El puntero al pcb que se quiere serializar.
+ * buffer : El buffer donde se almacenaran los bytes del pcb.
+ * buffer_size : El tamanio del buffer, que terminara siendo el total de bytes que componen al pcb.
+ */
 void serialize_pcb(t_pcb *pcb, char **buffer, t_size *buffer_size) {
 
     serialize_data(&pcb->pid, sizeof(pcb->pid), buffer, buffer_size);
@@ -14,4 +19,20 @@ void serialize_pcb(t_pcb *pcb, char **buffer, t_size *buffer_size) {
     serialize_data(&pcb->instrucciones_serializado, pcb->instrucciones_size, buffer, buffer_size);
 }
 
+/*
+ * Deserializa un pcb a una cadena de bytes.
+ *
+ * pcb : El puntero al pcb que se habra deserializado.
+ * serialized_data : El buffer donde estaran almacenados los bytes del pcb.
+ * serialized_data_size : La cantidad de bytes almacenados en serialized_data.
+ */
+void deserialize_pcb(t_pcb **pcb, char **serialized_data, t_size *serialized_data_size) {
 
+    deserialize_data(&(*pcb)->pid, sizeof((*pcb)->pid), serialized_data, serialized_data_size);
+    deserialize_data(&(*pcb)->program_counter, sizeof((*pcb)->program_counter), serialized_data, serialized_data_size);
+    deserialize_data(&(*pcb)->program_counter, sizeof((*pcb)->stack_pointer), serialized_data, serialized_data_size);
+    deserialize_stack(&(*pcb)->stack_index, serialized_data, serialized_data_size);
+    deserialize_data(&(*pcb)->status, sizeof((*pcb)->status), serialized_data, serialized_data_size);
+    deserialize_data(&(*pcb)->instrucciones_size, sizeof((*pcb)->instrucciones_size), serialized_data, serialized_data_size);
+    deserialize_data(&(*pcb)->instrucciones_serializado, (*pcb)->instrucciones_size, serialized_data, serialized_data_size);
+}
--- vac-commons/pcb/pcb.h
+++ vac-commons/pcb/pcb.h
@@ -15,11 +15,11 @@ typedef struct {
     uint32_t stack_pointer;
     t_list stack_index;
     enum_queue status;
-    t_intructions instrucciones_serializado;
     t_size instrucciones_size;
+    t_intructions instrucciones_serializado;
 } t_pcb;
 
-void serialize_enum_queue(enum_queue status, char **pString);
 void serialize_pcb(t_pcb *pcb, char **buffer, t_size *buffer_size);
+void deserialize_pcb(t_pcb **pcb, char **serialized_data, t_size *serialized_data_size);
 
 #endif //SERIALIZATION_PCB_H
