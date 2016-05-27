bb0a088d63f4a2bb62ee2b83a47a45510d006cf2
Alan Vaudagna <alanvaudagna@gmail.com>
2016-05-27 13:01:00 -0300

    Testing.

--- probando_metadata/serialize_test.c
+++ probando_metadata/serialize_test.c
@@ -12,10 +12,21 @@ int main (void) {
     int c = 7777;
     u_int32_t  d = 8888;
 
-    char * buff = NULL;//malloc(sizeof(int));
-    char * cursor = buff;
-    cursor = serialize_data(&a, sizeof(a), &buff);
-    printf("a: %d\n", atoi(buff));
+    char * buff = malloc(1);
+    buff = serialize_data(&a, sizeof(a), &buff);
+    printf("a: %d\n", (int) *buff);
+
+    buff = serialize_data(&b, sizeof(b), &buff);
+    printf("b: %d\n", (u_int32_t) *(buff+sizeof(a)));
+
+
+    buff = serialize_data(&c, sizeof(c), &buff);
+    printf("c: %d\n", (int) *(buff+sizeof(b)+sizeof(a)));
+/*
+    buff = serialize_data(&d, sizeof(d), &buff);
+    printf("d: %d\n", (u_int32_t) *(buff+sizeof(b)+sizeof(a)+sizeof(c)));
+
+    printf("todo: %s\n", buff);*/
 
     return 0;
 }
