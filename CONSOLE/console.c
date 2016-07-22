7df43a32d20aaa39759fc87c22ace7498be131b4
Alan Vaudagna <alanvaudagna@gmail.com>
2016-07-21 23:03:40 -0300

    Console fixes.

--- CONSOLE/console.c
+++ CONSOLE/console.c
@@ -1,7 +1,8 @@
 #include<stdio.h> //printf
 #include<string.h>    //strlen
 #include<sys/socket.h>    //socket
-#include "socketCommons/socketCommons.h"
+#include "libs/socketCommons.h"
+#include "libs/serialize.h"
 #include <signal.h>
 #include <commons/config.h>
 #include <commons/log.h>
@@ -23,7 +24,7 @@ int main(int argc, char *argv[]) {
 
 
 	int kernelSocketClient;
-	char *kernel_reply = calloc(1, 4);
+	void * kernel_reply = NULL;
 	//create kernel client
 	if (argc != 3) {
 		puts("usage: console console.config ansisopFile");
@@ -50,89 +51,97 @@ int main(int argc, char *argv[]) {
 
 	fseek(fp, SEEK_SET, 0);
 	fseek(fp, 0L, SEEK_END);
-	long int sz = ftell(fp);
+	long int file_length = ftell(fp);
 	fseek(fp, 0L, SEEK_SET);
 
-	char hash_bang[100];
+	char * hash_bang = calloc(1, sizeof(char) * 100);
 	fgets(hash_bang, 100, fp);
-	sz = sz - strlen(hash_bang) - 1;
+	file_length = file_length - strlen(hash_bang) - 1;
 
-	char *prog = (char *) calloc(1, sz);
+	char *prog = (char *) calloc(1, (size_t) (file_length));
 
-	fread(prog, sz + 1, 1, fp);
+	fread(prog, (size_t) file_length, 1, fp);
 	fclose(fp);
 
-	int sizeMsj = strlen("0") + 5 + (int) sz;
-	char *mensaje = (char *) calloc(1, sizeMsj);
+    int mensaje_index = 0;
+	char operacion = '0';
+	void * mensaje = NULL;
+    serialize_data(&operacion, sizeof(char), &mensaje, &mensaje_index);
+    serialize_data(&file_length, sizeof(int), &mensaje, &mensaje_index);
+    serialize_data(prog, (size_t) file_length, &mensaje, &mensaje_index);
 
-	char buffer[20];
-	sprintf(buffer, "%04d", (int) sz);
-
-	strcpy(mensaje, "0");
-	strcat(mensaje, buffer);
-	strcat(mensaje, prog);
-	send(kernelSocketClient, mensaje, sizeMsj, 0);
-	printf("El tamanio del programa es %s, y se envio al kernel: %s", buffer, mensaje);
+	send(kernelSocketClient, mensaje, (size_t) mensaje_index, 0);
+	log_info(console_log, "El tamanio del programa es %d, y se envio al kernel: %s", (int) file_length, prog);
 
 	free(prog);
 	free(mensaje);
 
-	if (recv(kernelSocketClient, kernel_reply, 4, 0) < 0) {
+    kernel_reply = calloc(1,sizeof(int));
+	if (recv(kernelSocketClient, kernel_reply, sizeof(int), 0) < 0) {
 		log_error(console_log, "Kernel no responde");
 		return EXIT_FAILURE;
-	} else if (!strcmp(kernel_reply, "0000")) {
-		log_error(console_log, "Kernel contesto: %s.No hay espacio en memoria para ejecutar el programa", kernel_reply);
+	}
+    int kRep, kRep_index = 0;
+    deserialize_data(&kRep, sizeof(int), kernel_reply, &kRep_index);
+    if (kRep == 0) {
+		log_error(console_log, "Kernel contesto: %d.No hay espacio en memoria para ejecutar el programa", kRep);
 	} else {
-		log_info(console_log, "El pid de la consola es %d. Se esta ejecutando el programa.", atoi(kernel_reply));
+		log_info(console_log, "El pid de la consola es %d. Se esta ejecutando el programa.", kRep);
 	}
     free(kernel_reply);
 
 	void *kernelBuffer = NULL;
-    int valor = 0;
-    int textLen = 0;
+    int valor = 0, valor_index = 0;
+    int textLen = 0, textLen_index = 0;
 	bool continua = true;
-    void *kernel_operation = NULL;
-	while (continua) {
-        kernel_operation = calloc(1, 1);
-		if (recv(kernelSocketClient, kernel_operation, 1, 0) > 0) {
-			log_info(console_log, "Kernel dijo: %d", atoi(kernel_operation));
-			switch (atoi(kernel_operation)) {
-				case 0:// Program END
+    char *kernel_operation = NULL;
+    while (continua) {
+        kernel_operation = calloc(1, sizeof(char));
+		if (recv(kernelSocketClient, kernel_operation, sizeof(char), 0) > 0) {
+			log_info(console_log, "Kernel dijo: %c", kernel_operation);
+			switch (*kernel_operation) {
+				case '0':// Program END
 					continua = false;
 					log_info(console_log, "Vamo a recontra calmarno. El programa finalizó correctamente");
 					break;
-				case 1:// Print value
+				case '1':// Print value
 					//recibo 4 bytes -> valor_variable
-                    kernelBuffer = calloc(1,4);
-					recv(kernelSocketClient, kernelBuffer, 4, 0);
-                    valor = atoi(kernelBuffer);
+                    kernelBuffer = calloc(1,sizeof(int));
+					recv(kernelSocketClient, kernelBuffer, sizeof(int), 0);
+                    deserialize_data(&valor, sizeof(int), kernelBuffer, &valor_index);
+                    valor_index = 0;
 					log_info(console_log, "El valor de la variable es: %d", valor);
 					free(kernelBuffer);
 					break;
 
-				case 2:// Print text
+				case '2':// Print text
 					//recibo 4 del tamaño + texto
-                    kernelBuffer = calloc(1,4);
-					recv(kernelSocketClient, kernelBuffer, 4, 0);
-					textLen = atoi(kernelBuffer);
+                    kernelBuffer = calloc(1,sizeof(int));
+					recv(kernelSocketClient, kernelBuffer, sizeof(int), 0);
+                    deserialize_data(&textLen, sizeof(int), kernelBuffer, &textLen_index);
+                    textLen_index = 0;
                     free(kernelBuffer);
                     if(textLen > 0) {
-                        kernelBuffer = calloc(1, textLen);
-                        recv(kernelSocketClient, kernelBuffer, textLen, 0);
+                        kernelBuffer = calloc(1, (size_t) textLen);
+                        recv(kernelSocketClient, kernelBuffer, (size_t) textLen, 0);
                         log_info(console_log, "%s.", kernelBuffer);
                     } else {
                         log_error(console_log, "Se recibio un tamanio de texto invalido");
                     }
                     free(kernelBuffer);
 					break;
+                default:
+                    log_error(console_log, "Operacion invalida recibida");
+                    free(kernel_operation);
+                    return -1;
 			}
 		} else {
             continua = false;
             log_error(console_log, "El programa finalizo repentinamente");
         }
+        free(kernel_operation);
 	}
 
-	free(kernel_operation);
 	close(kernelSocketClient);
 	log_info(console_log, "Se cerro la conexion con el kernel");
 	puts("Terminated console.");
